4c2e90278743279f5131c97b508b5454ba304b45
li-zhouzhou <2521170001@qq.com>
2022-09-07 21:14:14 +0800

    add main.cpp_002

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main()
 {
+    cout<<"aaaaaaaaaaaaaaaa"<<endl;
+
     Server server;//客户端的函数类
 
     int listen_fd = server.Creat_socket();
     int client_fd = server.wait_client(listen_fd);
     server.handle_client(listen_fd, client_fd);
+
+    cout<<"lizhoulihzou"<<endl;
 
     return 0;
 }
